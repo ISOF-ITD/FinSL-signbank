13c61a7d97eea125bbf830167774d3e0e62d3e2a
Per Låås <per.laas@sprakochfolkminnen.se>
2019-01-25 15:00:00 +0100

    OS-299 Lägga signbank i SiteVision hemsidan. Several fixes to public translation interface. TranslationAutoCompleteView objects.raw sql-like with parameter with solution from https://groups.google.com/forum/#!topic/django-users/bma8Qk2T-V4. Lots of sql tests with new objects.raw not yet removed.

--- signbank/dictionary/publicviews.py
+++ signbank/dictionary/publicviews.py
@@ -25,16 +25,68 @@ from django.views.generic.edit import FormView
 import json
 from django.http import HttpResponse
 
+import re
+
 # *code in view which returns json data *
 class TranslationAutoCompleteView(FormView):
     def get(self,request,*args,**kwargs):
         data = request.GET
-        name = data.get("term")
+        term = data.get("term")
+        #Remove not litterals as extra percausion
+        name = re.sub(u'[^\wåäö]', "", term)
+        # name2 = re.sub(r"[^a-zA-Zå-öÄ-Ö]+", "", term)
         if name:
+            # Return keywords
             # glosses = Gloss.objects.filter(idgloss__istartswith = name)
-            translations = Keyword.objects.filter(text__istartswith = name)
-        else:
-            translations = Keyword.objects.all()
+            # translations = Keyword.objects.filter(text__istartswith = name)
+
+            # Return ONLY keywords with published glosses
+            sqlselect = '''SELECT dk.id as id, dk.text as text FROM teckensprak.dictionary_keyword dk'''
+            sqljoin = ''' JOIN teckensprak.dictionary_translation dt ON dk.id = dt.keyword_id JOIN teckensprak.dictionary_gloss dg ON dg.id = dt.gloss_id'''
+            # sqljoin = ''' JOIN teckensprak.dictionary_translation dt ON dk.id = dt.keyword_id
+                                                    # JOIN teckensprak.dictionary_gloss dg ON dg.id = dt.gloss_id'''
+                                                    # WHERE dg.published = 1
+                                                    # AND upper(dk.text) like upper(%s)'''
+            # sqlfilter2 = ''' WHERE dg.published = 1 AND upper(dk.text) like upper(%s)'''
+            # sqlfilter2like = " WHERE dg.published = 1 AND text like '" + name + "%';"
+            sqlfilter2 = " WHERE dg.published = 1 AND text REGEXP '^" + name + "';"
+            # sqlfilter1 = " WHERE text REGEXP '^" + name + "';"
+            # sqlfilterparamlike = ''' WHERE dg.published = 1 AND upper(dk.text) like upper(%s)'''
+            # sqlfilterparam = " WHERE dg.published = 1 AND text REGEXP '^" + name + "';"
+            # sqlfilterparam = " WHERE dg.published = 1 AND text REGEXP '^%s';"
+
+            # https: // groups.google.com / forum /  # !topic/django-users/bma8Qk2T-V4
+            sqlfilter2 = " WHERE dg.published = 1 AND text like %s"
+            user_input = "%s%%" % name
+
+            # funkar:
+            sql = sqlselect + sqljoin + sqlfilter2
+            # sql = sqlselect + sqlfilter1
+
+            # funkar inte:
+            # sql = sqlselect + sqljoin + sqlfilter2like
+            # sql = sqlselect + sqljoin + sqlfilterparam
+
+            # sql1 = 'SELECT dk.id as id, dk.text as text FROM teckensprak.dictionary_keyword dk WHERE upper(dk.text) like upper(''' + name + '%'')'
+            # sql1 = 'SELECT dk.id as id, dk.text as text FROM teckensprak.dictionary_keyword dk WHERE upper(dk.text) like upper("fö%")'
+            # sql1 = "SELECT dk.id as id, dk.text as text FROM teckensprak.dictionary_keyword dk WHERE upper(dk.text) like upper("fö%") WHERE text REGEXP '^före';"
+
+            # sql1 = "SELECT dk.id as id, dk.text as text FROM teckensprak.dictionary_keyword dk WHERE text REGEXP '^%s';"
+            # sql1 = 'SELECT dk.id as id, dk.text as text FROM teckensprak.dictionary_keyword dk WHERE text REGEXP "^%s";'
+            # funkar inte:
+            # sql1 = "SELECT dk.id as id, dk.text as text FROM teckensprak.dictionary_keyword dk WHERE dk.text like 'fö%'"
+            # Unable to get repr for < class 'django.db.models.query.RawQuerySet' > 'SELECT dk.id as id, dk.text as text FROM teckensprak.dictionary_keyword dk WHERE dk.text like \\'fö%\\''
+            # sql1 = 'SELECT dk.id as id, dk.text as text FROM teckensprak.dictionary_keyword dk WHERE dk.text like "fö%"'
+            # funkar:
+            # sql1 = "SELECT dk.id as id, dk.text as text FROM teckensprak.dictionary_keyword dk WHERE text REGEXP '^" + name + "';"
+            # sql1 = 'SELECT dk.id as id, dk.text as text FROM teckensprak.dictionary_keyword dk WHERE text = "förening"'
+            # sql1 = 'SELECT dk.id as id, dk.text as text FROM teckensprak.dictionary_keyword dk'
+
+            # Return ONLY keywords with published glosses using objects.raw (no need for extra sql-view or model class)
+            translations = Keyword.objects.raw(sql, [user_input])
+            # translations = Keyword.objects.raw(sql)
+        # else:
+            # translations = Keyword.objects.all()
         results = []
         for translation in translations:
             all_json = {}
@@ -55,25 +107,16 @@ class TranslationListPublicView(ListView):
         # Call the base implementation first to get a context
         context = super(TranslationListPublicView, self).get_context_data(**kwargs)
         context["searchform"] = GlossPublicSearchForm(self.request.GET)
-        context["signlanguages"] = SignLanguage.objects.filter(id__in=[x.signlanguage.id for x in Dataset.objects.filter(is_public=True)])
+        context["signlanguages"] = SignLanguage.objects.filter(dataset__is_public=True).distinct()
+        context["signlanguage_count"] = context["signlanguages"].count()
         context["lang"] = self.request.GET.get("lang")
         if context["lang"]:
             context["searchform"].fields["dataset"].queryset = context["searchform"].fields["dataset"].queryset.filter(signlanguage__language_code_3char=context["lang"])
+        context["datasets"] = self.request.GET.getlist("dataset")
         context["first_letters"] = Gloss.objects.filter(dataset__is_public=True, published=True)\
             .annotate(first_letters=Substr(Upper('idgloss'), 1, 1)).order_by('first_letters')\
             .values_list('first_letters').distinct()
-
-        # ISOF Per
-        # tags_list = Tag.objects.filter(approved=0).order_by('-date')[:30]
-        tag_list = Tag.objects.all()
-        context["tags"] = tag_list
-        # application_root = applicationRoot(self.request)
-        # context["application_root"] = application_root
-        # populate_tags_for_object_list(context['object_list'], model=self.object_list.model)
-
-        # context['gloss_choices'] = Gloss.objects.filter(dataset=self.request.GET.get('dataset'))
-        context['gloss_choices'] = Gloss.objects.all()
-
+        context['lexicons'] = Dataset.objects.filter(is_public=True)
         return context
 
     def get_queryset(self):
@@ -95,16 +138,24 @@ class TranslationListPublicView(ListView):
         if vals != []:
             qs = qs.filter(dataset__in=vals)
 
-        if 'search' in get and get['search'] != '':
-            val = get['search']
-
-            # ISOF test
-            category = get.get('tags', 'Alla')
-
+        if 'gloss' in get and get['gloss'] != '':
+            val = get['gloss']
             # Filters
-            qs = qs.filter(Q(idgloss__istartswith=val) | Q(translation__keyword__text__istartswith=val)
-                           # | Q(idgloss_en__icontains=val) # idgloss_en not shown in results, therefore removed.
-                           )
+            # qs = qs.filter(Q(idgloss__istartswith=val))
+            qs = qs.filter(Q(idgloss__istartswith=val) | Q(translation__keyword__text__istartswith=val))
+        if 'keyword' in get and get['keyword'] != '':
+            val = get['keyword']
+            # Filters
+            # qs = qs.filter(translation__keyword__text__istartswith=val)
+            qs = qs.filter(Q(idgloss__istartswith=val) | Q(translation__keyword__text__istartswith=val))
+
+        # ISOF test
+        category = get.get('tags', 'Alla')
+
+        # Filters
+        # qs = qs.filter(Q(idgloss__istartswith=val) | Q(translation__keyword__text__istartswith=val)
+                       # | Q(idgloss_en__icontains=val) # idgloss_en not shown in results, therefore removed.
+                       # )
 
         # From class GlossListView(ListView) in adminviews.py
         if 'tags' in get and get['tags'] != '':
@@ -131,18 +182,12 @@ class TranslationListPublicView(ListView):
         else:
             qs = qs.order_by('idgloss')
 
-        # If needed set used translation_lang
-        # translation_lang = get_language()
-        translation_lang = 'sv'
-
+        qs = qs.select_related('dataset')
         # Prefetching translation and dataset objects for glosses to minimize the amount of database queries.
-        qs = qs.prefetch_related(Prefetch('translation_set', queryset=Translation.objects.filter(
-            language__language_code_2char__iexact=translation_lang).select_related('keyword')),
-                                 Prefetch('glosstranslations_set', queryset=GlossTranslations.objects.filter(
-                                     language__language_code_2char__iexact=translation_lang)),
-                                 Prefetch('dataset'),
-                                 # Ordering by version to get the first versions posterfile.
-                                 Prefetch('glossvideo_set', queryset=GlossVideo.objects.all().order_by('version')))
+        qs = qs.prefetch_related(Prefetch('glosstranslations_set'),
+                                 Prefetch('glosstranslations_set__language'),
+                                 # Make sure we only show GlossVideos that have 'is_public=True'
+                                 Prefetch('glossvideo_set', queryset=GlossVideo.objects.filter(is_public=True)))
         return qs
 
 class GlossListPublicView(ListView):
