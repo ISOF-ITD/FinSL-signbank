6596761bd6345162265f860d07932ab8bf52c505
perlaasisof <per.laas@sprakochfolkminnen.se>
2020-10-19 15:48:23 +0200

    Ajustment for test env.

--- signbank/settings/configuration.py
+++ signbank/settings/configuration.py
@@ -4,11 +4,11 @@ import platform
 class Configuration():
     BEAR_DEVELOPMENT_COMPUTER_NAME = 'BearUbuntuTest'
     FRIGG_NAME = 'uuc-isof005.its.uu.se'
-    FRIGG_TEST_NAME = 'uuc-isof004-t.its.uu.se'
+    FRIGG_TEST_NAME = 'sql2-t.its.uu.se'
 
     LOCALHOST_ADDRESS = 'localhost'
-    ODEN_ADDRESS = 'oden.sprakochfolkminnen.se'
-    ODEN_TEST_ADDRESS = 'oden-test.sprakochfolkminnen.se'
+    ODEN_ADDRESS = 'oden.isof.se'
+    ODEN_TEST_ADDRESS = 'oden-test.isof.se'
 
     computer = platform.node()
     debug = not (computer == FRIGG_NAME)
