f38031cbbe09de418881fd4a280f9fba9c1dfb3c
Per Låås <per.laas@isof.se>
2024-05-21 16:46:23 +0200

    Show map for ortnamn-tecken

--- signbank/dictionary/admin_isof.py
+++ signbank/dictionary/admin_isof.py
@@ -9,14 +9,14 @@ from .models_isof import Ortnamn_tecken
 
 class Ortnamn_teckenAdmin(VersionAdmin):
     # Making sure these fields are not edited in admin
-    readonly_fields = ('createdate', 'createdby', 'changedate', 'editedby',)
+    readonly_fields = ('id','createdate', 'createdby', 'changedate', 'editedby','map_tag',)
     actions = []
 
-    fields = ['id', 'ortnamn', 'forklaring_svenska', 'forklaring_tecken',
+    fields = [('id', 'ortnamn'), ('lat', 'lng'), 'forklaring_svenska', 'forklaring_tecken',
             'teckenfilnamn',
               'teckenfilnamn2',
               'teckenfilnamn3',
-              ('createdate', 'createdby', 'changedate', 'editedby')]
+              ('createdate', 'createdby', 'changedate', 'editedby'), 'map_tag']
 
     save_on_top = True
     # save_as = True
@@ -24,3 +24,10 @@ class Ortnamn_teckenAdmin(VersionAdmin):
     search_fields = ['^ortnamn']
     # list_filter = ('dataset', 'published', 'exclude_from_ecv', TagListFilter, )
 
+    def save_model(self, request, obj, form, change):
+        if change == True:  # Was the post changed?
+            obj.editedby = request.user  # Get the current user and log it as the editor
+        else:  # The post might be new
+            if getattr(obj, 'createdby', None) is None:  # check if the creator is defined
+                obj.createdby = request.user  # If not, set the current user as the creator
+        obj.save()  # Save the post.
--- signbank/dictionary/models_isof.py
+++ signbank/dictionary/models_isof.py
@@ -4,6 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.utils.safestring import mark_safe
+from string import Template
+
 # Invisible space used to change default django admin alphabetic order of models
 LEVEL1 = u"\u200B"
 LEVEL2 = u"\u200B" + u"\u200B"
@@ -38,6 +41,49 @@ class Ortnamn_tecken(models.Model):
                              verbose_name="Skapad av")
     editedby = models.ForeignKey(User, db_column='editedby', null=True, blank=True, editable=False, on_delete=models.DO_NOTHING,
                                  related_name='Uppdaterad av+', verbose_name="Uppdaterad av")
+
+    def map_tag(self):
+        values = {
+            'lat': str(self.lat),
+            'lng': str(self.lng)
+        }
+        map_template = """
+            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css" />
+            <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"></script>
+            <script type="text/javascript" src="https://unpkg.com/leaflet.vectorgrid@1.2.0"></script>
+            <div id="socken_map" style="width: 100%; height: 400px;"></div>
+            <script type="text/javascript">
+                var map = L.map("socken_map").setView([${lat}, ${lng}], 8); // Skapar en karta
+                L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {attribution: "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"}).addTo(map);
+
+                var layer = 'SockenStad_ExtGranskning-clipped:SockenStad_ExtGranskn_v1.0_clipped'
+                var isoftilesUrl = 'https://oden.isof.se/geoserver/gwc/service/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&LAYER=' + layer + '&STYLE=&TILEMATRIX=EPSG:900913:{z}&TILEMATRIXSET=EPSG:900913&FORMAT=application/x-protobuf;type=mapbox-vector&TILECOL={x}&TILEROW={y}'
+                var isofVectorTileOptions = {
+                    rendererFactory: L.canvas.tile,
+                    maxZoom: 14
+                };
+                var isofPbfLayer = L.vectorGrid.protobuf(isoftilesUrl, isofVectorTileOptions).addTo(map);
+
+                var marker = L.marker([${lat}, ${lng}]); // Lägger till en L.marker
+                marker.addTo(map);
+
+                map.on('click', function(e) { // Map click handler
+                    marker.setLatLng(e.latlng); // Uppdaterar punkten på kartan
+
+                    django.jQuery('#id_lat').val(e.latlng.lat.toFixed(6)); // Skriver e.latlng.lat från punkter var man klickade på kartan till "lat" fältet
+                    django.jQuery('#id_lng').val(e.latlng.lng.toFixed(6)); // Skriver e.latlng.lng från punkter var man klickade på kartan till "lng" fältet
+                });
+            </script>
+        """
+
+        template = Template(map_template)
+
+        map_html = template.substitute(values)
+
+        return mark_safe(map_html);
+
+    map_tag.short_description = 'Karta'
+    map_tag.allow_tags = True
 
     class Meta:
         db_table = 'ortnamn_tecken'
